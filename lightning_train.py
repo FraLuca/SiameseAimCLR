a174c37e059b43a63ec6356f3073c8913b1628a2
paolomandica <paolomandica.p@gmail.com>
2022-04-04 13:04:41 +0000

    Add pytorch lightning implementation of BYOL AimCLR

---
+++ lightning_train.py
@@ -0,0 +1,175 @@
+import torch
+import multiprocessing as mp
+import argparse
+import yaml
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from torch.utils.data import DataLoader
+
+from feeder.ntu_feeder import Feeder_triple
+from net.byol_aimclr_lightning import BYOLAimCLR
+from net.st_gcn_no_proj import Model as STGCN
+
+
+def load_config(arg):
+    # load YAML config file as dict
+    with open(arg.config, 'r') as f:
+        default_arg = yaml.load(f, Loader=yaml.FullLoader)
+
+    # load new parser with default arguments
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.set_defaults(**default_arg)
+    try:
+        cfg = parser.parse_args('')
+    except Exception as e:
+        print(e)
+
+    # build sub-parsers
+    for k, value in cfg._get_kwargs():
+        if isinstance(value, dict):
+            new_parser = argparse.ArgumentParser(add_help=False)
+            new_parser.set_defaults(**value)
+            cfg.__setattr__(k, new_parser.parse_args(''))
+
+    return cfg
+
+
+class SelfSupervisedLearner(pl.LightningModule):
+    def __init__(self, base_encoder, cfg):
+        super().__init__()
+        self.model = BYOLAimCLR(base_encoder, vars(cfg.model_args))
+        self.cfg = cfg
+        # log hyperparams to wandb
+        self.save_hyperparameters()
+
+    def forward(self, batch):
+        [data1, data2, data3], label = batch
+
+        data1 = data1.float()
+        data2 = data2.float()
+        data3 = data3.float()
+        label = label.long()
+
+        if self.cfg.stream == 'joint':
+            pass
+        elif self.cfg.stream == 'motion':
+            motion1 = torch.zeros_like(data1)
+            motion2 = torch.zeros_like(data2)
+            motion3 = torch.zeros_like(data3)
+
+            motion1[:, :, :-1, :, :] = data1[:, :, 1:, :, :] - data1[:, :, :-1, :, :]
+            motion2[:, :, :-1, :, :] = data2[:, :, 1:, :, :] - data2[:, :, :-1, :, :]
+            motion3[:, :, :-1, :, :] = data3[:, :, 1:, :, :] - data3[:, :, :-1, :, :]
+
+            data1 = motion1
+            data2 = motion2
+            data3 = motion3
+        elif self.cfg.stream == 'bone':
+            Bone = [(1, 2), (2, 21), (3, 21), (4, 3), (5, 21), (6, 5), (7, 6), (8, 7), (9, 21),
+                    (10, 9), (11, 10), (12, 11), (13, 1), (14, 13), (15, 14), (16, 15), (17, 1),
+                    (18, 17), (19, 18), (20, 19), (21, 21), (22, 23), (23, 8), (24, 25), (25, 12)]
+
+            bone1 = torch.zeros_like(data1)
+            bone2 = torch.zeros_like(data2)
+            bone3 = torch.zeros_like(data3)
+
+            for v1, v2 in Bone:
+                bone1[:, :, :, v1 - 1, :] = data1[:, :, :,
+                                                  v1 - 1, :] - data1[:, :, :, v2 - 1, :]
+                bone2[:, :, :, v1 - 1, :] = data2[:, :, :,
+                                                  v1 - 1, :] - data2[:, :, :, v2 - 1, :]
+                bone3[:, :, :, v1 - 1, :] = data3[:, :, :,
+                                                  v1 - 1, :] - data3[:, :, :, v2 - 1, :]
+
+            data1 = bone1
+            data2 = bone2
+            data3 = bone3
+        else:
+            raise ValueError
+
+        if self.current_epoch <= self.cfg.mining_epoch:
+            loss1, loss2, loss3 = self.model(data1, data2, data3, nnm=False)
+            loss = loss1.mean() + (loss2.mean() + loss3.mean()) / 2.  # the mean does the average per each gpu
+
+            if hasattr(self.model, 'module'):
+                self.model.module.update_ptr(self.cfg.batch_size)
+            else:
+                self.model.update_ptr(self.cfg.batch_size)
+        else:
+            loss1, loss2, loss3, loss4, loss5, loss6 = self.model(
+                data1, data2, data3, nnm=True, topk=self.cfg.topk)
+            loss_a = loss1.mean() + (loss2.mean() + loss3.mean()) / 2.  # the mean does the average per each gpu
+            loss_b = loss4.mean() + (loss5.mean() + loss6.mean()) / 2.  # the mean does the average per each gpu
+            loss = (1 - self.cfg.lambda_mining) * loss_a + (self.cfg.lambda_mining) * loss_b
+
+            if hasattr(self.model, 'module'):
+                self.model.module.update_ptr(self.cfg.batch_size)
+            else:
+                self.model.update_ptr(self.cfg.batch_size)
+
+        return loss
+
+    def training_step(self, batch, _):
+        loss = self.forward(batch)
+        self.log('loss', loss, on_step=True, on_epoch=True)
+        return {'loss': loss}
+
+    def configure_optimizers(self):
+        # return torch.optim.Adam(self.parameters(), lr=LR)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.cfg.base_lr, momentum=0.9,
+                                    nesterov=self.cfg.nesterov, weight_decay=float(self.cfg.weight_decay))
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=self.cfg.step, gamma=0.1)
+        return [optimizer], [lr_scheduler]
+
+    def on_before_zero_grad(self, _):
+        if self.model.use_momentum:
+            self.model.update_moving_average()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='byol-lightning-test')
+    parser.add_argument('-c', '--config', type=str, required=True,
+                        default='/data_volume/SiameseAimCLR/config/ntu60/pretext/prova.yaml')
+    arg = parser.parse_args()
+
+    cfg = load_config(arg)
+
+    # data loading
+    train_feeder = Feeder_triple(cfg.train_feeder_args.data_path,
+                                 cfg.train_feeder_args.label_path)
+    train_loader = DataLoader(
+        dataset=train_feeder,
+        batch_size=cfg.batch_size,
+        shuffle=True,
+        pin_memory=True,    # set True when memory is abundant
+        num_workers=mp.cpu_count()//3,
+        persistent_workers=True,
+        drop_last=True)
+
+    wandb_logger = None
+    if not cfg.disable_wandb:
+        # init wandb logger
+        wandb_logger = WandbLogger(project='byol_aimclr', group='dev')
+
+    # init self-supervised learner
+    model = SelfSupervisedLearner(STGCN, cfg)
+
+    if wandb_logger is not None:
+        wandb_logger.watch(model, log_freq=10)
+
+    # init trainer
+    trainer = pl.Trainer(
+        gpus=cfg.device,
+        max_epochs=cfg.num_epoch,
+        accumulate_grad_batches=1,
+        sync_batchnorm=True,
+        strategy='ddp',
+        num_nodes=1,
+        logger=wandb_logger,
+        default_root_dir=cfg.work_dir)
+
+    # start training
+    trainer.fit(model, train_loader)
---
+++ net/byol_aimclr_lightning.py
@@ -0,0 +1,443 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+import copy
+import random
+from functools import wraps
+
+
+# helper functions
+
+def default(val, def_val):
+    return def_val if val is None else val
+
+
+def flatten(t):
+    return t.reshape(t.shape[0], -1)
+
+
+def singleton(cache_key):
+    def inner_fn(fn):
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            instance = getattr(self, cache_key)
+            if instance is not None:
+                return instance
+
+            instance = fn(self, *args, **kwargs)
+            setattr(self, cache_key, instance)
+            return instance
+        return wrapper
+    return inner_fn
+
+
+def get_module_device(module):
+    return next(module.parameters()).device
+
+
+def set_requires_grad(model, val):
+    for p in model.parameters():
+        p.requires_grad = val
+
+# loss fn
+
+
+def loss_fn(x, y, loss_name='cosine_sim'):
+
+    if loss_name == 'cosine_sim':
+        x = F.normalize(x, dim=-1, p=2)
+        y = F.normalize(y, dim=-1, p=2)
+        return 2 - 2 * (x * y).sum(dim=-1)
+
+    elif loss_name == 'mse':
+        loss = nn.MSELoss()
+        return loss(x, y).mean(dim=-1)
+
+
+# augmentation utils
+
+
+class RandomApply(nn.Module):
+    def __init__(self, fn, p):
+        super().__init__()
+        self.fn = fn
+        self.p = p
+
+    def forward(self, x):
+        if random.random() > self.p:
+            return x
+        return self.fn(x)
+
+# exponential moving average
+
+
+class EMA():
+    def __init__(self, beta):
+        super().__init__()
+        self.beta = beta
+
+    def update_average(self, old, new):
+        if old is None:
+            return new
+        return old * self.beta + (1 - self.beta) * new
+
+
+def update_moving_average(ema_updater, ma_model, current_model):
+    for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
+        old_weight, up_weight = ma_params.data, current_params.data
+        ma_params.data = ema_updater.update_average(old_weight, up_weight)
+
+# MLP class for projector and predictor
+
+
+class MLP(nn.Module):
+    def __init__(self, dim, projection_size):
+        super().__init__()
+
+        n_layers = len(projection_size)
+        layer_list = []
+
+        for idx, next_dim in enumerate(projection_size):
+
+            if idx == n_layers-1:
+                layer_list.append(nn.Linear(dim, next_dim))
+            else:
+                layer_list.append(nn.Linear(dim, next_dim))
+                layer_list.append(nn.BatchNorm1d(next_dim))
+                layer_list.append(nn.ReLU(inplace=True))
+                dim = next_dim
+
+        self.net = nn.Sequential(*layer_list)
+
+    def forward(self, x):
+        return self.net(x)
+
+
+# a wrapper class for the base neural network
+# will manage the interception of the hidden layer output
+# and pipe it into the projecter and predictor nets
+
+class NetWrapper(nn.Module):
+    def __init__(self, net, projection_hidden_size, layer=-2):
+        super().__init__()
+        self.net = net
+        self.layer = layer
+
+        self.projector = None
+        # self.projection_size = projection_size
+        self.projection_hidden_size = projection_hidden_size
+
+        self.hidden = {}
+        self.hook_registered = False
+
+    def _find_layer(self):
+        if type(self.layer) == str:
+            modules = dict([*self.net.named_modules()])
+            return modules.get(self.layer, None)
+        elif type(self.layer) == int:
+            children = [*self.net.children()]
+            return children[self.layer]
+        return None
+
+    def _hook(self, _, input, output):
+        device = input[0].device
+        self.hidden[device] = flatten(output)
+
+    def _register_hook(self):
+        layer = self._find_layer()
+        assert layer is not None, f'hidden layer ({self.layer}) not found'
+        handle = layer.register_forward_hook(self._hook)
+        self.hook_registered = True
+
+    @singleton('projector')
+    def _get_projector(self, hidden):
+        _, dim = hidden.shape
+        projector = MLP(dim, self.projection_hidden_size)
+        return projector.to(hidden)
+
+    def get_representation(self, x, drop=False):
+        if self.layer == -1:
+            return self.net(x, drop)
+
+        if not self.hook_registered:
+            self._register_hook()
+
+        self.hidden.clear()
+        _ = self.net(x, drop)
+        hidden = self.hidden[x.device]
+        self.hidden.clear()
+
+        assert hidden is not None, f'hidden layer {self.layer} never emitted an output'
+        return hidden
+
+    def forward(self, x, return_projection=True, drop=False):
+        if drop:
+            representation, representation_drop = self.get_representation(x, drop)
+        else:
+            representation = self.get_representation(x, drop)
+
+        if not return_projection:
+            return representation
+
+        # just for the instanciation of the singleton
+        projector = self._get_projector(representation)
+
+        if drop:
+            projection_ext = projector(representation)
+            projection_drop = projector(representation_drop)
+            return projection_ext, projection_drop
+        else:
+            projection = projector(representation)
+            return projection
+
+
+# main class
+class BYOLAimCLR(nn.Module):
+    def __init__(
+        self, base_encoder=None, pretrain=True, queue_size=32768,
+        time_dim=50, spat_dim=25, in_channels=3, hidden_channels=64, out_channels=1024,  # encoder parameters
+        # projector and predictor parameters
+        hidden_layer=-1, projection_hidden_size=[4096, 1024], predictor_hidden_size=[1024, 1024],
+        moving_average_decay=0.999, use_momentum=True,  # momentum update parameters
+        # other encoder parameters
+        dropout=0.5, graph_args={'layout': 'ntu-rgb+d', 'strategy': 'spatial'}, edge_importance_weighting=True,
+        loss_name='cosine_sim', **kwargs
+    ):
+        super().__init__()
+
+        self.loss_name = loss_name
+        encoder_type = base_encoder
+
+        self.pretrain = pretrain
+
+        if not self.pretrain:
+            net = base_encoder(in_channels=in_channels, hidden_channels=hidden_channels, out_channels=out_channels,
+                                dropout=dropout, graph_args=graph_args,
+                                edge_importance_weighting=edge_importance_weighting, **kwargs)
+            self.online_encoder = NetWrapper(
+                net, projection_hidden_size, layer=hidden_layer)
+        else:
+            net = base_encoder(in_channels=in_channels, hidden_channels=hidden_channels, out_channels=out_channels,
+                                dropout=dropout, graph_args=graph_args,
+                                edge_importance_weighting=edge_importance_weighting, **kwargs)
+            # self.net = net
+            self.online_encoder = NetWrapper(
+                net, projection_hidden_size, layer=hidden_layer)
+
+            self.online_predictor = MLP(projection_hidden_size[-1], predictor_hidden_size)
+
+            self.use_momentum = use_momentum
+            self.target_encoder = None
+            self.target_ema_updater = EMA(moving_average_decay)
+
+            # create the queue
+            self.K = queue_size
+            self.register_buffer("queue", torch.randn(projection_hidden_size[-1], queue_size))
+            self.queue = F.normalize(self.queue, dim=0)
+            self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+
+        # get device of network and make wrapper same device
+        device = get_module_device(net)
+        self.to(device)
+        # send a mock image tensor to instantiate singleton parameters
+        self.forward(torch.randn(2, in_channels, time_dim, spat_dim, 2, device=device), torch.randn(
+            2, in_channels, time_dim, spat_dim, 2, device=device), torch.randn(2, in_channels, time_dim, spat_dim, 2, device=device))
+
+    @singleton('target_encoder')
+    def _get_target_encoder(self):
+        target_encoder = copy.deepcopy(self.online_encoder)
+        set_requires_grad(target_encoder, False)
+        return target_encoder
+
+    def reset_moving_average(self):
+        del self.target_encoder
+        self.target_encoder = None
+
+    def update_moving_average(self):
+        assert self.use_momentum, 'you do not need to update the moving average, since you have turned off momentum for the target encoder'
+        assert self.target_encoder is not None, 'target encoder has not been created yet'
+        update_moving_average(self.target_ema_updater, self.target_encoder, self.online_encoder)
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys):
+        batch_size = keys.shape[0]
+        ptr = int(self.queue_ptr)
+        gpu_index = keys.device.index
+        self.queue[:, (ptr + batch_size * gpu_index):(ptr + batch_size * (gpu_index + 1))] = keys.T
+
+    @torch.no_grad()
+    def update_ptr(self, batch_size):
+        assert self.K % batch_size == 0  # for simplicity
+        self.queue_ptr[0] = (self.queue_ptr[0] + batch_size) % self.K
+
+    def forward(
+        self,
+        image_one_extreme=None, image_one=None, image_two=None,
+        return_projection=True,
+        nnm=False, topk=1
+    ):
+        assert not (
+            self.training and image_one.shape[0] == 1), 'you must have greater than 1 sample when training, due to the batchnorm in the projection layer'
+
+        if nnm:
+            return self.nearest_neighbors_mining(image_one, image_two, image_one_extreme, topk)
+
+        if not self.pretrain:
+            return self.online_encoder(image_one, return_projection=return_projection)
+
+        online_proj_one = self.online_encoder(image_one)
+        online_proj_two = self.online_encoder(image_two)
+
+        online_pred_one = self.online_predictor(online_proj_one)
+        online_pred_two = self.online_predictor(online_proj_two)
+
+        if image_one_extreme != None:
+            online_proj_one_ext, online_proj_one_ext_drop = self.online_encoder(
+                image_one_extreme, drop=True)
+            online_pred_one_ext = self.online_predictor(online_proj_one_ext)
+            online_pred_one_ext_drop = self.online_predictor(online_proj_one_ext_drop)
+
+        with torch.no_grad():
+            target_encoder = self._get_target_encoder() if self.use_momentum else self.online_encoder
+            target_proj_one = target_encoder(image_one)
+            target_proj_two = target_encoder(image_two)
+            target_proj_one.detach_()
+            target_proj_two.detach_()
+
+            if image_one_extreme != None:
+                target_proj_one_ext, target_proj_one_ext_drop = target_encoder(
+                    image_one_extreme, drop=True)
+                target_proj_one_ext.detach_()
+                target_proj_one_ext_drop.detach_()
+
+        # Online VS Target
+        loss_one = loss_fn(online_pred_one, target_proj_two.detach(), loss_name=self.loss_name)
+        loss_two = loss_fn(online_pred_two, target_proj_one.detach(), loss_name=self.loss_name)
+        loss = loss_one + loss_two
+        loss = loss.mean()
+
+        loss_ext = None
+        loss_ext_drop = None
+        if image_one_extreme != None:
+            # Online_Extreme VS Target
+            loss_one_ext = loss_fn(online_pred_one_ext,
+                                   target_proj_two.detach(), loss_name=self.loss_name)
+            loss_two_ext = loss_fn(
+                online_pred_two, target_proj_one_ext.detach(), loss_name=self.loss_name)
+            loss_ext = loss_one_ext + loss_two_ext
+            loss_ext = loss_ext.mean()
+
+            # Online_Extreme_Drop VS Target
+            loss_one_ext_drop = loss_fn(online_pred_one_ext_drop,
+                                        target_proj_two.detach(), loss_name=self.loss_name)
+            loss_two_ext_drop = loss_fn(
+                online_pred_two, target_proj_one_ext_drop.detach(), loss_name=self.loss_name)
+            loss_ext_drop = loss_one_ext_drop + loss_two_ext_drop
+            loss_ext_drop = loss_ext_drop.mean()
+
+        if not target_proj_one.shape[0] == 2:
+            self._dequeue_and_enqueue(target_proj_one)
+
+        return loss, loss_ext, loss_ext_drop
+
+    def nearest_neighbors_mining(self, image_one, image_two, image_one_extreme, topk):
+
+        online_proj_one = self.online_encoder(image_one)
+        online_proj_two = self.online_encoder(image_two)
+
+        l_neg = torch.einsum('nc,ck->nk', [online_proj_one, self.queue.clone().detach()])
+
+        online_pred_one = self.online_predictor(online_proj_one)
+        online_pred_two = self.online_predictor(online_proj_two)
+
+        if image_one_extreme != None:
+            online_proj_one_ext, online_proj_one_ext_drop = self.online_encoder(
+                image_one_extreme, drop=True)
+            online_pred_one_ext = self.online_predictor(online_proj_one_ext)
+            online_pred_one_ext_drop = self.online_predictor(online_proj_one_ext_drop)
+
+            l_neg_e = torch.einsum('nc,ck->nk', [online_proj_one_ext, self.queue.clone().detach()])
+            l_neg_ed = torch.einsum(
+                'nc,ck->nk', [online_proj_one_ext_drop, self.queue.clone().detach()])
+
+        with torch.no_grad():
+            target_encoder = self._get_target_encoder() if self.use_momentum else self.online_encoder
+            target_proj_one = target_encoder(image_one)
+            target_proj_two = target_encoder(image_two)
+            target_proj_one.detach_()
+            target_proj_two.detach_()
+
+            if image_one_extreme != None:
+                target_proj_one_ext, target_proj_one_ext_drop = target_encoder(
+                    image_one_extreme, drop=True)
+                target_proj_one_ext.detach_()
+                target_proj_one_ext_drop.detach_()
+
+        # Online VS Target
+        loss_one = loss_fn(online_pred_one, target_proj_two.detach(), loss_name=self.loss_name)
+        loss_two = loss_fn(online_pred_two, target_proj_one.detach(), loss_name=self.loss_name)
+        loss = loss_one + loss_two
+        loss = loss.mean()
+
+        loss_ext = None
+        loss_ext_drop = None
+        if image_one_extreme != None:
+            # Online_Extreme VS Target
+            loss_one_ext = loss_fn(online_pred_one_ext,
+                                   target_proj_two.detach(), loss_name=self.loss_name)
+            loss_two_ext = loss_fn(
+                online_pred_two, target_proj_one_ext.detach(), loss_name=self.loss_name)
+            loss_ext = loss_one_ext + loss_two_ext
+            loss_ext = loss_ext.mean()
+
+            # Online_Extreme_Drop VS Target
+            loss_one_ext_drop = loss_fn(online_pred_one_ext_drop,
+                                        target_proj_two.detach(), loss_name=self.loss_name)
+            loss_two_ext_drop = loss_fn(
+                online_pred_two, target_proj_one_ext_drop.detach(), loss_name=self.loss_name)
+            loss_ext_drop = loss_one_ext_drop + loss_two_ext_drop
+            loss_ext_drop = loss_ext_drop.mean()
+
+        # nearest neighbors mining to expand the positive set
+        _, topkdix = torch.topk(l_neg, topk, dim=1)
+        _, topkdix_e = torch.topk(l_neg_e, topk, dim=1)
+        _, topkdix_ed = torch.topk(l_neg_ed, topk, dim=1)
+
+        new_positive = self.queue.T[topkdix.reshape(-1)]
+        new_positive_ext = self.queue.T[topkdix_e.reshape(-1)]
+        new_positive_ext_drop = self.queue.T[topkdix_ed.reshape(-1)]
+
+        online_pred_new = self.online_predictor(new_positive)
+        online_pred_new_ext = self.online_predictor(new_positive_ext)
+        online_pred_new_ext_drop = self.online_predictor(new_positive_ext_drop)
+
+        # Online VS Target
+        loss_one_new = loss_fn(online_pred_new, target_proj_two.detach(), loss_name=self.loss_name)
+        loss_two_new = loss_fn(online_pred_new, target_proj_one.detach(), loss_name=self.loss_name)
+        loss_new = loss_one_new + loss_two_new
+        loss_new = loss_new.mean()
+
+        loss_ext_new = None
+        loss_ext_drop_new = None
+        if image_one_extreme != None:
+            # Online_Extreme VS Target
+            loss_one_ext_new = loss_fn(
+                online_pred_new_ext, target_proj_two.detach(), loss_name=self.loss_name)
+            loss_two_ext_new = loss_fn(
+                online_pred_new_ext, target_proj_one.detach(), loss_name=self.loss_name)
+            loss_ext_new = loss_one_ext_new + loss_two_ext_new
+            loss_ext_new = loss_ext_new.mean()
+
+            # Online_Extreme_Drop VS Target
+            loss_one_ext_drop_new = loss_fn(
+                online_pred_new_ext_drop, target_proj_two.detach(), loss_name=self.loss_name)
+            loss_two_ext_drop_new = loss_fn(
+                online_pred_new_ext_drop, target_proj_one.detach(), loss_name=self.loss_name)
+            loss_ext_drop_new = loss_one_ext_drop_new + loss_two_ext_drop_new
+            loss_ext_drop_new = loss_ext_drop_new.mean()
+
+        self._dequeue_and_enqueue(target_proj_one)
+
+        return loss, loss_ext, loss_ext_drop, loss_new, loss_ext_new, loss_ext_drop_new
